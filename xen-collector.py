df8e951699b3e5dde93e5b3935aae14a39501443
arbenatti <arbenatti@gmail.com>
2015-06-18 18:05:57 -0300

    updated

--- xen-collector.py
+++ xen-collector.py
@@ -1,4 +1,28 @@
 #!/usr/bin/python
+#
+# Copyright (c) 2015 Alexander Rafael Benatti <arbenatti@gmail.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer
+#    in this position and unchanged.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import XenAPI, re, json
 from docopt import docopt
@@ -20,13 +44,6 @@ Options:
     -n <name>, --name <name>                    Name of the object, e.g. XEN hostname, datastore URL, etc.
     -p <properties>, --properties <properties>  Name of the property as defined by the vSphere Web SDK
     -k <key>, --key <key>                       Provide additional key for data filtering
-
-Examples:
-    xen-collector.py -m vm.discover -V vc01.example.org
-    xen-collector.py -m vm.discover -V vc01.example.org -p runtime.powerState
-    xen-collector.py -m vm.get -V vc01.example.org -n vm01.example.org -p summary.overallStatus
-    xen-collector.py -m vm.diks.get -V vc01.example.org -n vm01.example.org -k /var
-    xen-collector.py -m vm.process.get -V vc01.example.org -n vm01.example.org -U user -P pass
 
 """
 
